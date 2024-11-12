a5173de8c357c1de5a3723f10fb6a012bcf5c798
Roky <apple@Rokys-MacBook-Air.local>
2024-11-12 22:58:10 +0900

    Ticket fair solved

--- busFareCalculator.js
+++ busFareCalculator.js
@@ -6,3 +6,20 @@ Ticket fare Calculator
     - Senior citizens (age >= 60) gets a 15% Discount
     - Otherwise Regular ticket fare 800 tk
 */
+
+function ticketFair(age) {
+    const regularFare = 800;
+    let fare;
+    if (age < 10) {
+        fare = 0;
+    } else if (age >= 60) {
+        fare = regularFare * 0.85;
+    } else if (age >= 10 && age <= 25) {
+        fare = regularFare * 0.5;
+    }else{
+        fare = regularFare;
+    }
+    return `The ticket fare is ${fare} Tk.`;
+}
+let age = 50;
+console.log(ticketFair(age));
