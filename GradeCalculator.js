f5cbb023579a3291335f28ba0c5c62339df99190
Roky <apple@Rokys-MacBook-Air.local>
2024-11-13 21:14:28 +0900

    simple grade calculator solved

--- GradeCalculator.js
+++ GradeCalculator.js
@@ -11,3 +11,37 @@ Create a simple JavaScript program that takes a student's score as input and ret
     F: 0-59
 
 ***/
+const readline = require('readline')
+
+// Set up readline to read from terminal
+const readFromTerminal = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readFromTerminal.question("Enter the student's score (0-100): ", (input) => {
+    const score = Number(input);
+
+
+    function gradeCalculator(score) {
+        if (score >= 90 && score <= 100) {
+            return "A";
+        } else if (score >= 80 && score <= 89) {
+            return "B";
+        } else if (score >= 70 && score <= 79 ){
+            return "C";
+        } else if (score >= 60 && score <= 69){
+            return "D";
+        }else if (score >= 0 && score <= 59){
+            return "F";
+        }else{
+            return "Please Enter a valid number between 0 to 100."
+        }
+    }
+    let grade = gradeCalculator(score);
+    console.log("The student's grade is:", grade);
+
+    readFromTerminal.close(); // Close the input stream
+});
+
+
